787b1280c861b868890885da7722d2b37d8ae19b
dheerajputtapaka <46364365+dheerajputtapaka@users.noreply.github.com>
2024-02-09 11:23:27 -0500

    Update web_interface.py

--- web_interface.py
+++ web_interface.py
@@ -38,7 +38,7 @@ def predict_news(news):
     unseen_seq = torch.tensor(tokens_unseen['input_ids'])
     unseen_mask = torch.tensor(tokens_unseen['attention_mask'])
 
-    clf = joblib.load("Model/fakenews_model.sav")
+    clf = joblib.load("Model/latest_model.sav")
     with torch.no_grad():
         preds = clf(unseen_seq, unseen_mask)
         preds = preds.detach().cpu().numpy()
